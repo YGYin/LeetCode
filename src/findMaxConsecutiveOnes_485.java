31e6bd6c7cb2804213f176eeb30872144298b118
YGYin <ygyin0307@gmail.com>
2022-07-01 15:31:54 +0800

    Day 3

---
+++ src/findMaxConsecutiveOnes_485.java
@@ -0,0 +1,15 @@
+public class findMaxConsecutiveOnes_485 {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0, result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1)
+                ++count;
+            else {
+                result = Math.max(result, count);
+                count = 0;
+            }
+        }
+        result = Math.max(result, count);
+        return result;
+    }
+}
