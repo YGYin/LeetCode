2779ff64e791b54ffd6f495070b4d7244090d977
YGYin <ygyin0307@gmail.com>
2022-07-07 23:59:13 +0800

    Day 5

---
+++ src/FindDiagonalOrder_498.java
@@ -0,0 +1,43 @@
+public class FindDiagonalOrder_498 {
+    public int[] findDiagonalOrder(int[][] mat) {
+        if (mat == null || mat.length == 0)
+            return new int[]{};
+
+        int row = mat.length, col = mat[0].length;
+        int[] res = new int[row * col];
+        int x = 0, y = 0, index = 0;
+
+        // i: The number of diagonal line
+        for (int i = 0; i < row + col - 1; ++i) {
+            // Direction: right top
+            if (i % 2 == 0) {
+                while (y >= 0 && x < col) {
+                    res[index++] = mat[y][x];
+                    --y;
+                    ++x;
+                }
+                // At boundary
+                if (x < col)
+                    ++y;
+                else {
+                    y += 2;
+                    --x;
+                }
+            } else {
+                while (y < row && x >= 0) {
+                    res[index++] = mat[y][x];
+                    ++y;
+                    --x;
+                }
+                // At boundary
+                if (y < row)
+                    ++x;
+                else {
+                    ++y;
+                    x += 2;
+                }
+            }
+        }
+        return res;
+    }
+}
