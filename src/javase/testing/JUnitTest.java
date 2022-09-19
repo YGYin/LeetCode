9b61a3624cec6306fa2368ecb11044f8ffb985b3
YGYin <ygyin0307@gmail.com>
2022-09-19 12:03:03 +0800

    Test Update

---
+++ src/javase/testing/JUnitTest.java
@@ -0,0 +1,20 @@
+package javase.testing;
+
+import org.junit.jupiter.api.Test;
+
+public class JUnitTest {
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public void method1() {
+        System.out.println("method1 is invoked.");
+    }
+
+    @Test
+    public void method2() {
+        System.out.println("method2 is invoked.");
+    }
+
+}
