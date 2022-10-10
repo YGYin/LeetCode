7f2f7e6b16b6ea504cc619897f2f7876eff4080b
YGYin <ygyin0307@gmail.com>
2022-10-10 15:14:39 +0800

    array traversal

---
+++ src/array/traversal/MaximumProduct_628.java
@@ -0,0 +1,40 @@
+package array.traversal;
+
+import java.util.Arrays;
+
+public class MaximumProduct_628 {
+    // O(NlogN)
+    public int maximumProduct(int[] nums) {
+        if (nums.length == 3)
+            return nums[0] * nums[1] * nums[2];
+        int n = nums.length;
+        Arrays.sort(nums);
+        return Math.max(nums[0] * nums[1] * nums[n - 1], nums[n - 1] * nums[n - 2] * nums[n - 3]);
+    }
+
+    // O(N)
+    public int maximumProduct_2(int[] nums) {
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+        for (int x : nums) {
+            if (x < min1) {
+                min2 = min1;
+                min1 = x;
+            } else if (x < min2)
+                min2 = x;
+
+            if (x > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = x;
+            } else if (x > max2) {
+                max3 = max2;
+                max2 = x;
+            } else if (x > max3)
+                max3 = x;
+
+        }
+
+        return Math.max(min1 * min2 * max1, max1 * max2 * max3);
+    }
+}
