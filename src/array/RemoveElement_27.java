3df41b5a901354edbe65b780191db83c8130822b
YGYin <ygyin0307@gmail.com>
2022-10-24 11:45:26 +0800

    array

---
+++ src/array/RemoveElement_27.java
@@ -0,0 +1,14 @@
+package array;
+
+public class RemoveElement_27 {
+
+    public int removeElement(int[] nums, int val) {
+        int left = 0, n = nums.length;
+        for (int right = 0; right < n; right++)
+            if (nums[right] != val)
+                nums[left++] = nums[right];
+
+        return left;
+    }
+
+}
