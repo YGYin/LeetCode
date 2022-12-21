0dd81f37bcfc91cd7e4dfd79fd0eeed4847e80c7
YGYin <ygyin0307@gmail.com>
2022-12-21 19:42:05 +0800

    Offer 03

---
+++ src/offer/FindRepeatNumber_03.java
@@ -0,0 +1,13 @@
+package offer;
+
+import java.util.Arrays;
+
+public class FindRepeatNumber_03 {
+    public int findRepeatNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; ++i)
+            if (nums[i] == nums[i + 1])
+                return nums[i];
+        return -1;
+    }
+}
