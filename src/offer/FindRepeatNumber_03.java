0f10ae30cedad69b7593af0ec498215934947200
YGYin <ygyin0307@gmail.com>
2022-12-23 17:47:54 +0800

    Offer 03, hash

--- src/offer/FindRepeatNumber_03.java
+++ src/offer/FindRepeatNumber_03.java
@@ -1,6 +1,7 @@
 package offer;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class FindRepeatNumber_03 {
     public int findRepeatNumber(int[] nums) {
@@ -8,6 +9,17 @@ public class FindRepeatNumber_03 {
         for (int i = 0; i < nums.length - 1; ++i)
             if (nums[i] == nums[i + 1])
                 return nums[i];
+        return -1;
+    }
+
+    // Hash, time: O(n) space: O(n)
+    public int findRepeatNumber_2(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            if (set.contains(i))
+                return i;
+            set.add(i);
+        }
         return -1;
     }
 }
