b713545d87bd965ebed24975cb9be1011607e4b3
YGYin <ygyin0307@gmail.com>
2022-12-26 15:56:46 +0800

    String builder replaces spaces

---
+++ src/offer/ReplaceSpace_05.java
@@ -0,0 +1,17 @@
+package offer;
+
+public class ReplaceSpace_05 {
+    public String replaceSpace(String s) {
+        if (s == null)
+            return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            if (c == ' ')
+                stringBuilder.append("%20");
+            else
+                stringBuilder.append(c);
+        }
+        return stringBuilder.toString();
+    }
+}
