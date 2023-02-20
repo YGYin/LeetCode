bbe273b593e21daa5ac298a04697c01d1d3bf00c
YGYin <ygyin0307@gmail.com>
2023-02-20 23:47:52 +0800

    Servlet Test

---
+++ TomcatDemo/src/main/java/com/github/ygyin/ServletDemo.java
@@ -0,0 +1,28 @@
+package com.github.ygyin;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet("/demo1")
+public class ServletDemo implements Servlet {
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("Servlet Hello World!");
+    }
+
+    public String getServletInfo() {
+        return null;
+    }
+
+    public void destroy() {
+
+    }
+}
