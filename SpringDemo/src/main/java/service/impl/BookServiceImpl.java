ebc1f6e36a394031fccaca733f300d2a02101921
YGYin <ygyin0307@gmail.com>
2023-02-27 17:53:54 +0800

    Spring DI Demo

--- SpringDemo/src/main/java/service/impl/BookServiceImpl.java
+++ SpringDemo/src/main/java/service/impl/BookServiceImpl.java
@@ -4,14 +4,20 @@ import dao.BookDao;
 import dao.impl.BookDaoImpl;
 import service.BookService;
 
-import java.awt.print.Book;
 
 public class BookServiceImpl implements BookService {
-    private BookDao bookDao = new BookDaoImpl();
+    // DI: 1. 删除业务层中使用 new 方式创建的 dao 对象
+    // private BookDao bookDao = new BookDaoImpl();
+    private BookDao bookDao;
 
     @Override
     public void save() {
         System.out.println("Book Service Save...");
         bookDao.save();
+    }
+
+    // DI: 2. 提供依赖对象对应的 set 方法，供容器传递对象
+    public void setBookDao(BookDao bookDao) {
+        this.bookDao = bookDao;
     }
 }
