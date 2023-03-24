4e0c4fa802a62fb61f194b89055b581a280c2f4e
YGYin <ygyin0307@gmail.com>
2023-03-24 13:17:16 +0800

    Test

---
+++ src/app.java
@@ -0,0 +1,8 @@
+public class app {
+    public static void main(String[] args) {
+        Integer userId = 1, goodsId = 2;
+        String st = "RandomString";
+        String s = userId.toString() + goodsId.toString();
+        System.out.println(s);
+    }
+}
