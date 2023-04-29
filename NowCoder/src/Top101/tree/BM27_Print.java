96fe4b0ea293923acafa312e7a051db46cd06737
YGYin <ygyin0307@gmail.com>
2023-04-29 15:12:37 +0800

    BM27 按之字形顺序打印二叉树

---
+++ NowCoder/src/Top101/tree/BM27_Print.java
@@ -0,0 +1,43 @@
+package Top101.tree;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Queue;
+
+public class BM27_Print {
+    /*
+        思路与 BM26 二叉树层次遍历相同，加入 flag 判断用于反转单行数组
+        单行反转，双行不反转
+        时间复杂度: O(n)，其中 n 为二叉树的节点数，每个节点访问一次
+        空间复杂度: O(n)，队列的空间为二叉树的一层的节点数，最坏情况二叉树的一层为 O(n)
+     */
+    public ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        if (pRoot == null)
+            return res;
+        // 新建队列用于层次遍历，将根节点加入队列
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(pRoot);
+        boolean flag = true;
+        while (!queue.isEmpty()) {
+            ArrayList<Integer> row = new ArrayList<>();
+            // 奇数行反转，偶数行不反转
+            flag = !flag;
+            // 因先进入的是根节点，故每层节点多少，队列大小就是多少
+            int qSize = queue.size();
+            for (int i = 0; i < qSize; i++) {
+                TreeNode cur = queue.poll();
+                row.add(cur.val);
+                if (cur.left != null)
+                    queue.add(cur.left);
+                if (cur.right != null)
+                    queue.add(cur.right);
+            }
+            if (flag)
+                Collections.reverse(row);
+            res.add(row);
+        }
+        return res;
+    }
+}
