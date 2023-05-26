a31696099321f260dbfb211162ea4d3b18897a9a
YGYin <ygyin0307@gmail.com>
2023-05-26 15:32:44 +0800

    BM32 合并二叉树

---
+++ NowCoder/src/Top101/tree/BM32_MergeTrees.java
@@ -0,0 +1,14 @@
+package Top101.tree;
+
+public class BM32_MergeTrees {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null)
+            return t2;
+        if (t2 == null)
+            return t1;
+        TreeNode head = new TreeNode(t1.val + t2.val);
+        head.left = mergeTrees(t1.left, t2.left);
+        head.right = mergeTrees(t1.right, t2.right);
+        return head;
+    }
+}
