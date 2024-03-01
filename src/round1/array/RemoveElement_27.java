bb9da7ad00a2c045faec0fe6111870f7eee8b592
YGYin <ygyin0307@gmail.com>
2024-03-01 13:49:17 +0800

    Day 1 Array_1

--- src/round1/array/RemoveElement_27.java
+++ src/round1/array/RemoveElement_27.java
@@ -4,23 +4,23 @@ public class RemoveElement_27 {
     // 暴力
     // O(n^2)   O(1)
     public int removeElement_01(int[] nums, int val) {
-        int length = nums.length;
-        for (int i = 0; i < length; i++) {
-            // 发现要移除的元素，全部往前移一位
+        int size = nums.length;
+        for (int i = 0; i < nums.length; i++) {
             if (nums[i] == val) {
-                for (int j = i + 1; j < length; j++)
-                    nums[j - 1] = nums[j];
-                // 数组大小 - 1，且 i 回退一位
-                --length;
+                for (int j = i; j < nums.length - 1; j++)
+                    nums[j] = nums[j + 1];
+                --size;
+                // 因为下标 i 后面的元素都向前移动了一位
+                // 为了确保下一次循环 i 位于当前正确下标所以 --
                 --i;
             }
         }
-        return length;
+        return size;
     }
 
-    // 双指针
-    // 快指针遍历寻找是否存在要移除的元素
-    // 忙指针指向更新数组下标的位置
+    // 快慢双指针
+    // 快指针遍历寻找新数组的元素
+    // 慢指针指向更新 新数组下标的位置
     public int removeElement_02(int[] nums, int val) {
         int slowP = 0;
         for (int fastP = 0; fastP < nums.length; fastP++)
@@ -31,5 +31,30 @@ public class RemoveElement_27 {
                 nums[slowP++] = nums[fastP];
 
         return slowP;
+    }
+
+    // 相向双指针
+    // 可以确保移动最少元素，分别找左边等于 val 和找右边不等于 val 的进行交换
+    // 时间：O(n)
+    public int removeElement_03(int[] nums, int val) {
+        // 分别指向左右两端
+        int left = 0;
+        int right = nums.length - 1;
+        // 左右区间相等有意义
+        while (left <= right) {
+            // 找左边等于 val 的元素
+            while (left <= right && nums[left] != val)
+                ++left;
+            // 找右边不等于 val 的元素
+            while (left <= right && nums[right] == val)
+                --right;
+            // 交换前确保 left < right 且不相等，相等交换无意义
+            // 交换完移动下标
+            if (left < right)
+                nums[left++] = nums[right--];
+        }
+        // 最后返回左下标，左下标正好指向更新后数组的下一位
+        // 因为数组开始下标为 0，所以刚好为新数组的元素个数
+        return left;
     }
 }
--- src/round1/array/Search_704.java
+++ src/round1/array/Search_704.java
@@ -1,23 +1,68 @@
 package round1.array;
 
 public class Search_704 {
+//    public int search(int[] nums, int target) {
+//        int left = 0;
+//        int right = nums.length;
+//        int mid;
+//        // 左闭右开
+//        // left == right 时为不合法区间
+//        while (left < right) {
+//            mid = left + (right - left) / 2;
+//            // 已经判断过 nums[mid] < target
+//            // 左为闭区间，无需重复判断 mid
+//            if (nums[mid] < target)
+//                left = mid + 1;
+//                // 但左闭右开区间右边界取不到，所以可以等于 mid
+//            else if (nums[mid] > target)
+//                right = mid;
+//            else
+//                return mid;
+//        }
+//        return -1;
+//    }
+
+    // 左闭右开
     public int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length;
         int mid;
-        // 左闭右开
-        // left == right 时为不合法区间
+        // 左闭右开时，left == right 为不合法区间
         while (left < right) {
             mid = left + (right - left) / 2;
-            // 已经判断过 nums[mid] < target
-            // 左为闭区间，无需重复判断 mid
-            if (nums[mid] < target)
-                left = mid + 1;
-                // 但左闭右开区间右边界取不到，所以可以等于 mid
-            else if (nums[mid] > target)
-                right = mid;
-            else
+            if (nums[mid] == target)
                 return mid;
+                // 因为已判断过 mid < target
+                // left 的闭区间范围无需再覆盖 mid
+            else if (nums[mid] < target)
+                left = mid + 1;
+                // 因为已判断过 mid > target
+                // right 为开区间，right = mid 时本身不会覆盖 mid
+            else
+                right = mid;
+        }
+        return -1;
+    }
+
+    //左闭右闭
+    public int search_1(int[] nums, int target) {
+        int left = 0;
+        // 左闭右闭区间也要随之变化
+        int right = nums.length - 1;
+        int mid;
+        // 左闭右闭时，left == right 为合法区间
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+                return mid;
+                // 因为已判断过 mid < target
+                // left 的闭区间范围无需再覆盖 mid
+            else if (nums[mid] < target)
+                left = mid + 1;
+                // 因为已判断过 mid > target
+                // right 为开区间，right = mid 时本身不会覆盖 mid
+            else
+                right = mid - 1;
         }
         return -1;
     }
