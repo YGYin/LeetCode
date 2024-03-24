ebf3dea649433f24a3e89c9e02ea1b2539ef27a7
YGYin <ygyin0307@gmail.com>
2024-03-24 15:33:46 +0800

    Day 17 LRU_Cache

--- src/round1/LRUCache.java
+++ src/round1/LRUCache.java
@@ -4,85 +4,37 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LRUCache {
-    private DLLNode head;
-    private DLLNode tail;
+    int length;
+    int capacity;
     Map<Integer, Object[]> maps;
-    private int capacity;
-    private int length;
-
-
-    public LRUCache() {
-    }
+    DLLNode head;
+    DLLNode tail;
 
     public LRUCache(int capacity) {
-        // 初始化头尾节点，并连接头尾节点
+        // 初始化 head 和 tails，并连接
         head = new DLLNode(-1);
         tail = new DLLNode(-1);
         head.next = tail;
         tail.prev = head;
 
-        // 初始化 maps 和 capacity
-        maps = new HashMap<>();
         this.capacity = capacity;
+        maps = new HashMap<>();
     }
 
     public int get(int key) {
-        // 1. 先判断哈希表中是否存在 key
-        //  存在的话返回 key 的 val ，并将对应节点移到最前面即 head 的下一个节点
+        // 如果在缓存中
         if (maps.containsKey(key)) {
-            // 1.1 先从哈希表中获取节点
+            // 获取当前节点，然后将该节点先断开连接再移动到头部
             DLLNode curNode = (DLLNode) maps.get(key)[0];
-            // 记录该节点的前节点和后节点
-            // 将两者连接起来，等价于在双向链表中删除 curNode
             deleteNode(curNode);
-
-            // 1.2 将其插入到头节点后，需先记录头节点的后节点
-            // 连接上头节点和 curNode 以及 temp
             moveToHead(curNode);
             return (int) maps.get(key)[1];
         }
-        // key 不存在 maps 中
         return -1;
-
-    }
-
-    public void put(int key, int value) {
-        // 判断哈希表中是否存储了 key
-        // 如果存在，不仅需要返回 key 的 value，还需将其移动到 head 后面
-        if (maps.containsKey(key)) {
-            // 获取节点
-            DLLNode curNode = (DLLNode) maps.get(key)[0];
-            // 记录前后节点，连接起来删除 curNode
-            deleteNode(curNode);
-
-            moveToHead(curNode);
-            // 更新节点的值
-            maps.put(key, new Object[]{curNode, value});
-            return;
-        }
-
-        // 如果不包含该 key，先判断缓存容量是否满了
-        // 满了删除最后一个节点
-        if (length == capacity) {
-            DLLNode delNode = tail.prev;
-            deleteNode(delNode);
-            // 哈希表移除该节点的值 ！
-            maps.remove(delNode.val);
-            --length;
-        }
-        // 然后再将添加的节点移动到前面去
-        DLLNode curNode = new DLLNode(key);
-        moveToHead(curNode);
-        // 添加到哈希表中
-        maps.put(key, new Object[]{curNode, value});
-        ++length;
-        return;
     }
 
     private void moveToHead(DLLNode curNode) {
-        // 记录头节点下一节点
         DLLNode temp = head.next;
-        // 连接 head temp curNode
         head.next = curNode;
         curNode.prev = head;
         curNode.next = temp;
@@ -90,22 +42,47 @@ public class LRUCache {
     }
 
     private void deleteNode(DLLNode curNode) {
-        DLLNode prevNode = curNode.prev;
-        DLLNode nextNode = curNode.next;
-        prevNode.next = nextNode;
-        nextNode.prev = prevNode;
-    }
-}
-
-class DLLNode {
-    int val;
-    DLLNode prev;
-    DLLNode next;
-
-    public DLLNode() {
+        DLLNode prev = curNode.prev;
+        DLLNode next = curNode.next;
+        prev.next = next;
+        next.prev = prev;
     }
 
-    public DLLNode(int val) {
-        this.val = val;
+    public void put(int key, int value) {
+        // 先检查有没有
+        // 存在的话，移动到头部，然后更新值
+        if (maps.containsKey(key)) {
+            DLLNode curNode = (DLLNode) maps.get(key)[0];
+            deleteNode(curNode);
+            moveToHead(curNode);
+            maps.put(key, new Object[]{curNode, value});
+            return;
+        }
+        // 不存在就要新加节点，需要看缓存是否满了，满了的话先删除末尾
+        if (length == capacity) {
+            // 满了删除末尾节点，删除 maps 中映射关系
+            DLLNode delNode = tail.prev;
+            deleteNode(delNode);
+            maps.remove(delNode.val);
+            length--;
+        }
+        // 加入新节点到头部
+        DLLNode node = new DLLNode(key);
+        moveToHead(node);
+        maps.put(key, new Object[]{node, value});
+        length++;
+    }
+
+    private class DLLNode {
+        int val;
+        DLLNode prev;
+        DLLNode next;
+
+        public DLLNode() {
+        }
+
+        public DLLNode(int val) {
+            this.val = val;
+        }
     }
 }
