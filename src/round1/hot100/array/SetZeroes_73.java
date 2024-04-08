d66e8f06a987f871c9345ac44ef278262c1233a3
YGYin <ygyin0307@gmail.com>
2024-04-08 11:47:20 +0800

    Day 26 Hot100 Matrix_1

---
+++ src/round1/hot100/array/SetZeroes_73.java
@@ -0,0 +1,29 @@
+package round1.hot100.array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetZeroes_73 {
+    public void setZeroes(int[][] matrix) {
+        // 用两个 HashSet 记录哪些行和列有 0
+        Set<Integer> rowSet = new HashSet<>();
+        Set<Integer> colSet = new HashSet<>();
+        int rowLen = matrix.length;
+        int colLen = matrix[0].length;
+        // 扫两遍 matrix，第一遍用于记录哪些行和列有 0
+        for (int i = 0; i < rowLen; i++) {
+            for (int j = 0; j < colLen; j++) {
+                if (matrix[i][j] == 0) {
+                    rowSet.add(i);
+                    colSet.add(j);
+                }
+            }
+        }
+        // 第二遍设置 0
+        for (int i = 0; i < rowLen; i++)
+            for (int j = 0; j < colLen; j++)
+                if (rowSet.contains(i) || colSet.contains(j))
+                    matrix[i][j] = 0;
+
+    }
+}
---
+++ src/round1/hot100/array/SpiralOrder_54.java
@@ -0,0 +1,48 @@
+package round1.hot100.array;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SpiralOrder_54 {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        LinkedList<Integer> res = new LinkedList<>();
+        // 此处按题设可不判空
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+            return res;
+        // 定义上下左右四个边界
+        int up = 0, down = matrix.length - 1;
+        int left = 0, right = matrix[0].length - 1;
+
+        while (true) {
+            // 从左上角开始
+            // 先从左到右
+            for (int i = left; i <= right; i++)
+                res.add(matrix[up][i]);
+
+            // 准备从上到下，先移动一格并判断边界
+            if (++up > down)
+                break;
+            for (int i = up; i <= down; i++)
+                res.add(matrix[i][right]);
+
+            // 准备从左到右
+            if (--right < left)
+                break;
+            for (int i = right; i >= left; i--)
+                res.add(matrix[down][i]);
+
+            // 准备从下晚上，上一格
+            if (--down < up)
+                break;
+            for (int i = down; i >= up; i--) {
+                res.add(matrix[i][left]);
+            }
+            // 为下次从左到右边做准备
+            if (++left > right)
+                break;
+        }
+        return res;
+    }
+
+}
