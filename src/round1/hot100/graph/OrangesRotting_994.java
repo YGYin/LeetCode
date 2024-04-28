7e22ea4bbcb43019cd2f3083cfcc9e9fef3eb14d
YGYin <ygyin0307@gmail.com>
2024-04-28 13:00:35 +0800

    Day 38 Hot100 Graph updated

--- src/round1/hot100/graph/OrangesRotting_994.java
+++ src/round1/hot100/graph/OrangesRotting_994.java
@@ -59,20 +59,6 @@ public class OrangesRotting_994 {
         return fresh > 0 ? -1 : minute;
     }
 
-
-    public int orangesRotting_2(int[][] grid) {
-        // 腐烂的橘子，染色 bfs 模板
-        int minute = 0;
-        visited = new boolean[grid.length][grid[0].length];
-        int fresh = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 2)
-                    bfs(grid, i, j, minute);
-            }
-        }
-    }
-
     private void bfs(int[][] grid, int row, int col, int minute) {
         // 辅助队列
         Deque<int[]> queue = new ArrayDeque<>();
