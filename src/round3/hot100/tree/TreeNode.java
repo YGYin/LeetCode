3a79891f25784d70d33a9011981daf79d63cff78
YGYin <ygyin0307@gmail.com>
2024-11-06 21:05:35 +0800

    Round 3 Hot 100 Tree

---
+++ src/round3/hot100/tree/TreeNode.java
@@ -0,0 +1,14 @@
+package round3.hot100.tree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
