a450f0cf72a00d2388b5c7c52556e8e5e764d68f
YGYin <ygyin0307@gmail.com>
2025-03-16 16:03:44 +0800

    Round 4 DP

---
+++ src/round4/leetcode/dp/UniquePaths_62.java
@@ -0,0 +1,26 @@
+package round4.leetcode.dp;
+
+public class UniquePaths_62 {
+    /*
+        不同路径
+        1. dp[i][j]: 到达坐标 (i, j) 的不同路径数
+        2. dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        3. 因为只能向右或者向下移动，第一行和第一列初始为 1
+        4. 从左到右，从上到下
+     */
+    public int uniquePaths(int m, int n) {
+        // 定义并初始化 dp 数组
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++)
+            dp[i][0] = 1;
+        for (int j = 0; j < n; j++)
+            dp[0][j] = 1;
+
+        // 遍历 dp 数组
+        for (int i = 1; i < m; i++)
+            for (int j = 1; j < n; j++)
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+
+        return dp[m - 1][n - 1];
+    }
+}
